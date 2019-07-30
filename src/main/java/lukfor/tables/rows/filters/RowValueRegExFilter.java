1b78a00ce43937ebd3d460ed0030ce8ed1640521
Lukas Forer <lukas.forer@i-med.ac.at>
2019-07-30 16:47:22 +0200

    Fix issue in with missing values in RegEx filter

--- src/main/java/lukfor/tables/rows/filters/RowValueRegExFilter.java
+++ src/main/java/lukfor/tables/rows/filters/RowValueRegExFilter.java
@@ -4,19 +4,24 @@ import java.io.IOException;
 
 import lukfor.tables.rows.Row;
 
-public class RowValueRegExFilter implements IRowFilter{
+public class RowValueRegExFilter implements IRowFilter {
 
 	private String column;
-	
+
 	private String regex;
-	
+
 	public RowValueRegExFilter(String column, String regex) {
 		this.column = column;
 		this.regex = regex;
 	}
-	
+
 	public boolean accepts(Row row) throws IOException {
-		return row.getObject(column).toString().matches(regex);
+		Object value = row.getObject(column);
+		if (value != null) {
+			return value.toString().matches(regex);
+		} else {
+			return false;
+		}
 	}
 
 }
